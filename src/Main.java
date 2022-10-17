7b4c6658589ec1e54d04435eff2720b0dd92b362
Hunter Lawson <hunter.lawson@ufl.edu>
2022-10-17 19:01:36 -0400

    add client

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import models.Client;
+
+import java.io.*;
+import java.net.*;
+import java.nio.Buffer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Client client = new Client();
+
+        client.main();
     }
 }
