870ee4bbfea8aa86760131e4518c7be40e1ce7d9
Hunter Lawson <hunter.lawson@ufl.edu>
2022-10-21 18:29:14 -0400

    add exception comments

--- src/exceptions/AppConfigException.java
+++ src/exceptions/AppConfigException.java
@@ -1,5 +1,7 @@
 package exceptions;
 
+// This exception should be thrown whenever the application encounters an error while it is
+// initializing its values from the configuration files (in the App class constructor)
 public class AppConfigException extends Exception {
     public AppConfigException(String msg, Exception err) {
         super(msg, err);
--- src/exceptions/InvalidMessage.java
+++ src/exceptions/InvalidMessage.java
@@ -1,5 +1,6 @@
 package exceptions;
 
+// This message should be thrown when creating a message with invalid input
 public class InvalidMessage extends Exception {
     public InvalidMessage(String msg) {
         super(msg);
