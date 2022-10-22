696f54d4039d850c9573982b7fd770bccc871a6d
Hunter Lawson <hunter.lawson@ufl.edu>
2022-10-21 21:25:53 -0400

    add peer ID cl argument parsing

--- src/exceptions/ConnectionException.java
+++ src/exceptions/ConnectionException.java
@@ -2,11 +2,11 @@ package exceptions;
 
 // This exception should be thrown when any connection-related errors occur
 public class ConnectionException extends Exception {
-    ConnectionException(String msg, Exception e) {
+    public ConnectionException(String msg, Exception e) {
         super(msg, e);
     }
 
-    ConnectionException(String msg) {
+    public ConnectionException(String msg) {
         super(msg);
     }
 }
