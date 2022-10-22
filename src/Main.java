ed99a92f710c3b90788148cea4069c78a99de9e7
Hunter Lawson <hunter.lawson@ufl.edu>
2022-10-21 21:09:25 -0400

    add more error handling

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
 import controllers.App;
+import exceptions.ConnectionException;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(System.getProperty("user.dir"));
-        App app = App.getApp();
+        System.out.println("Working directory: " + System.getProperty("user.dir"));
+        System.out.println("Starting application - Loading configuration files");
 
-        app.run();
+        // Exceptions occurred when creating the application should be forwarded to the calling process
+        // These exceptions can't be handled at runtime because configuring the application should not fail
+        App app;
+        try {
+            app = App.getApp();
+        } catch(Exception e) {
+            throw e;
+        }
+
+        System.out.println("Starting application - Starting peer listener");
+
+        // Exceptions occurred when running the application can be caught depending on the type
+        try {
+            app.run();
+        } catch(ConnectionException e) {
+            throw new Exception("An error has occurred with the connection", e);
+        } catch(IOException e) {
+            throw new Exception("An error has occurred with the file system", e);
+        } catch(Exception e) {
+            throw e;
+        }
     }
 }
--- src/exceptions/AppConfigException.java
+++ src/exceptions/AppConfigException.java
@@ -6,6 +6,7 @@ public class AppConfigException extends Exception {
     public AppConfigException(String msg, Exception err) {
         super(msg, err);
     }
+
     public AppConfigException(String msg) {
         super(msg);
     }
